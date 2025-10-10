e67fb0590c25420153e1a1a16e47baf9c7a8121b
Anastasia Cherkasova <st140594@student.spbu.ru>
2025-10-10 21:16:23 +0300

    Implement sayHello function in hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include "hello.h"
+#include <iostream>
+void myproject :: sayHello () {
+	std :: cout << "Hello, world" << std::andl;
+	std::string s;
+	while (true){
+		std::cin >> s;
+		std::cout <<"Hello, " << s << "!" <<std :: endl;
+	}
+}
