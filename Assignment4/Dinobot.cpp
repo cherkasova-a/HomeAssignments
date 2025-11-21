392cd1c5d47839c610801a7c235e0d1175de1c58
Anastasia Cherkasova <st140594@student.spbu.ru>
2025-11-21 16:32:04 +0300

    push files

---
+++ Assignment4/Dinobot.cpp
@@ -0,0 +1,60 @@
+/*
+*Anastasia Cherkasova
+*st140594@student.spbu.ru
+*Assignment4
+*/
+#include "Dinobot.h"
+#include <iostream>
+
+Dinobot::Dinobot() : Transformer(), ferocity_(0), size_(0) {}
+Dinobot::Dinobot(const std::string& name, int level, int power, int speed, bool armor,
+                 const Engine& engine, Gun* gun, int ferocity, int size)
+    : Transformer(name, level, power, speed, armor, engine, gun), ferocity_(ferocity), size_(size) {}
+Dinobot::Dinobot(const Dinobot& other) : Transformer(other), ferocity_(other.ferocity_), size_(other.size_) {}
+Dinobot::~Dinobot() {}
+
+void Dinobot::setFerocity(int ferocity)
+{
+    ferocity_ = ferocity;
+}
+int Dinobot::getFerocity() const
+{
+    return ferocity_;
+}
+
+void Dinobot::setSize(int size)
+{
+    size_ = size;
+}
+int Dinobot::getSize() const
+{
+    return size_;
+}
+
+bool Dinobot::Move()
+{
+    std::cout << "Dinobot::Move\n";
+    return true;
+}
+bool Dinobot::Fire()
+{
+    std::cout << "Dinobot::Fire\n";
+    return true;
+}
+bool Dinobot::Transform()
+{
+    std::cout << "Dinobot::Transform\n";
+    return true;
+}
+
+void Dinobot::PrintInfo() const
+{
+    std::cout << "Dinobot::PrintInfo\n";
+}
+
+std::ostream& operator<<(std::ostream& os, const Dinobot& d)
+{
+    os << "Dinobot(" << static_cast<const Transformer&>(d)
+       << ", ferocity=" << d.ferocity_ << ", size=" << d.size_ << ")";
+    return os;
+}
---
+++ Assignment4/test_dinobot.cpp
@@ -0,0 +1,30 @@
+/*
+*Anastasia Cherkasova
+*st140594@student.spbu.ru
+*Assignment4
+*/
+#include "gtest/gtest.h"
+#include "Dinobot.h"
+
+TEST(DinobotTest, CtorsAndMethods)
+{
+    Engine eng("E", 300);
+    Gun gun("G", 20);
+    Dinobot di("Di", 3, 30, 7, true, eng, &gun, 99, 5);
+    ASSERT_EQ(di.getPower(), 30);
+    di.setFerocity(100);
+    ASSERT_EQ(di.getFerocity(), 100);
+
+    ASSERT_TRUE(di.Move());
+    ASSERT_TRUE(di.Fire());
+    ASSERT_TRUE(di.Transform());
+}
+
+TEST(DinobotTest, CopyCtor)
+{
+    Engine eng("E", 300);
+    Gun gun("G", 20);
+    Dinobot di("Di",3,30,7,true,eng,&gun,10,2);
+    Dinobot copy(di);
+    ASSERT_EQ(copy.getSize(), 2);
+}
